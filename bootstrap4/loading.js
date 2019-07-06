0f47fd9e08a4f6a9ce9d9a90415cb2a2c7d31aa9
Guccisan <32457819+Guccisan@users.noreply.github.com>
2019-07-06 16:54:32 +0900

    up1

---
+++ bootstrap4/loading.js
@@ -0,0 +1,13 @@
+$(function(){
+	var loader = $('#loader-wrapper');
+
+	//ページの読み込みが完了したらアニメーションを非表示
+	$(window).on('load',function(){
+		loader.fadeOut(1500);
+	});
+
+	//ページの読み込みが完了してなくても10秒後にアニメーションを非表示にする
+	setTimeout(function(){
+		loader.fadeOut(1500);
+	},10000);
+});
