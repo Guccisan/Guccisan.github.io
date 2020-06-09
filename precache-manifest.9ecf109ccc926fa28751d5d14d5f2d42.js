4aba386c98a75b44bc394b47bc5950d9ab80001c
Guccisan <s.kota5001@gmail.com>
2020-06-09 22:05:28 +0900

    Updates

---
+++ precache-manifest.9ecf109ccc926fa28751d5d14d5f2d42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76aae360afbcfb0196eef8901a12bda2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59ccc380cfa11235354f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b62b8a7071e68b270122",
+    "url": "/static/js/2.87eb8b5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.87eb8b5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ccc380cfa11235354f",
+    "url": "/static/js/main.9d066a10.chunk.js"
+  },
+  {
+    "revision": "759e81ee1e3f967c5c66",
+    "url": "/static/js/runtime-main.66c35898.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
