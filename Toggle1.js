9083bd6c542147210c215f3c2292fd889ed6da4c
Guccisan <32457819+Guccisan@users.noreply.github.com>
2021-01-31 23:51:37 +0900

    あ

--- Toggle1.js
+++ Toggle1.js
@@ -12,10 +12,10 @@ $(function() {
         $(this).toggleClass('active');
  
         if ($(this).hasClass('active')) {
-           $(this).find('.dropdown').css('display', 'block');
+           $('.dropdown',this).css('display', 'block');
            /* $('.dropdown').css('display', 'block');*/
         } else {
-            $(this).find('.dropdown').css('display', 'none');
+          $('.dropdown',this).css('display', 'none');
         }
     });
 });
